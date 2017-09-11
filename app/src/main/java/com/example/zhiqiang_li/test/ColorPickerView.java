b8d2689389745714be93d720d4b44cce090f9037
Alex <suifeng7319@163.com>
2017-09-11 09:27:40 +0800

    add sample framework

--- app/src/main/java/com/example/zhiqiang_li/test/ColorPickerView.java
+++ app/src/main/java/com/example/zhiqiang_li/test/ColorPickerView.java
@@ -1,12 +1,15 @@
 package com.example.zhiqiang_li.test;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -19,6 +22,14 @@ public class ColorPickerView extends View {
     private boolean switchPanel;
 
     private Rect mRect;
+
+    private OnClickListener listener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+    private Bitmap bitmap;
 
     public ColorPickerView(Context context) {
         this(context,null);
@@ -33,31 +44,65 @@ public class ColorPickerView extends View {
         init(context);
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-    }
-
     private void init(Context context) {
 
+        Paint mColorPaint = new Paint();
+        mColorPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mColorPaint.setStrokeWidth(1);
+        mColorPaint.setColor(Color.WHITE);
 
-        Paint mPaint = new Paint();
-        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        mPaint.setStrokeWidth(1);
-        mPaint.setColor(Color.WHITE);
+        mRect = new Rect();
+
+
+        Paint mHuePaint = new Paint();
+        mHuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mHuePaint.setStrokeWidth(1);
+        mHuePaint.setColor(Color.WHITE);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawPanel(canvas);
+
+        drawButton(canvas);
+
+        drawColorPanel(canvas);
+
+        drawHuePanel(canvas);
     }
 
-    private void drawPanel(Canvas canvas) {
+
+
+    //绘制按钮
+    private void drawButton(Canvas canvas) {
+//     canvas.drawBitmap(bitmap,);
+    }
+    // 绘制颜色选择面板
+    private void drawColorPanel(Canvas canvas) {
         // canvas.drawRoundRect();
     }
+    // 绘制色相选择面板
+    private void drawHuePanel(Canvas canvas) {
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    private void switchPanel(){
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
 
 
-    
+    }
 }
